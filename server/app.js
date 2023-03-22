5b9eaf651fda4cd0d66eafa80ae0fc71f704a3ff
Hassan-de <hassan3119863001@gmail.com>
2023-03-22 15:06:28 +0500

    Authentication system backend

---
+++ server/app.js
@@ -0,0 +1,46 @@
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+
+dotenv.config({path:'./config.env'});
+require('./db/conn');
+// const User = require('./model/userSchema'); 
+
+app.use(express.json());
+
+app.use(require('./router/auth'));
+
+const PORT = process.env.PORT;
+
+
+//Middleware
+
+const middleware = (req, res, next) => {
+    console.log('Hello middleware')
+    next();
+}
+
+// middleware();
+app.get('/', (req, res)=>
+{
+    res.send(`Hello world from app.js`)
+});
+
+app.get('/about',middleware , (req, res)=>
+{
+    res.send(`Hello world about`)
+});
+
+app.get('/signin', (req, res) => {
+    res.send("Hello from signin")
+});
+
+
+app.get('/signup', (req, res) => {
+    res.send("Hello from signup")
+});
+
+app.listen(PORT, () => {
+    console.log(`server is runnign at port no. https://localhost:${PORT}`)
+})
---
+++ server/router/auth.js
@@ -0,0 +1,118 @@
+const express = require('express')
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+require("../db/conn");
+const User = require("../model/userSchema"); 
+
+router.get('/', (req, res) => {
+    res.send(`Hello world from router`)
+});
+//using promises
+// router.post('/register', (req, res) => {
+//     const { name, email, phone, password, cpassword } = req.body;
+//     // console.log(name);
+//     // console.log(email);
+
+    
+//     // // res.json({message: req.body})
+//     // res.send("Register Page")
+
+//     if(!name || !email || !phone || !password || !cpassword ) {
+//         return res.status(422).json({error: "Fill the field properly"});
+//     }
+
+//     User.findOne({email:email}).then((userExist) => {
+//         if(userExist) {
+//             return res.status(422).json({error: "Email already exists"});
+//     }
+//         const user = new User({ name, email, phone, password, cpassword});
+//         user.save().then(() => {
+//             res.status(201).json({message: "User registered successfully"});
+//         }).catch((err) => res.status(500).json({error: "Failed to register user"}))
+//     }).catch(err => {console.log(err); });
+
+// });
+
+
+router.post('/register', async (req, res) => {
+    const { name, email, phone, password, cpassword } = req.body;
+    // console.log(name);
+    // console.log(email);
+
+    
+    // // res.json({message: req.body})
+    // res.send("Register Page")
+
+    if(!name || !email || !phone || !password || !cpassword ) {
+        return res.status(422).json({error: "Fill the field properly"});
+    }
+
+    try {
+        const userExist = await User.findOne({email:email});
+        
+        if(userExist) {
+            return res.status(422).json({error: "Email already exists"});
+        }
+        else if(password !== cpassword){
+            return res.status(422).json({error: "password are not matching"}); 
+        }
+        else{
+            const user = new User({ name, email, phone, password, cpassword});
+        await user.save();  
+        res.status(201).json({message: "User registered successfully"});
+        }
+
+    } catch(err)
+    {
+        console.log(err);
+    }
+});
+//         if (userRegister) {
+//             res.status(201).json({message: "User registered successfully"});
+//         } else {
+//             res.status(500).json({error: "Failed to register user"})
+//         }
+//     }catch(err) {
+//         console.log(err);
+//     }
+    
+// });
+
+//login route
+
+router.post("/signin", async  (req, res) => {
+    // console.log(req.body);
+    // res.json({message: "a message"});
+
+    try {
+        const {email,  password } = req.body;
+
+        if (!email || !password) {
+            return res.status(400).json({error:"Fill the fields properly"});
+        }
+
+        const userLogin = await User.findOne({ email: email  });
+        
+        // console.log(userLogin); 
+
+        if(userLogin) {            
+            const isMatch = await bcrypt.compare(password, userLogin.password );
+
+        if(!isMatch){
+            res.status(400).json({error: "Invalid Credentials Password"});
+        }else{
+            res.json({message: "User signed in successfully"});
+        }    
+        } else {
+            res.status(400).json({error: "Invalid Credentials"});
+        }
+        
+
+    }catch(err){
+        console.log(err);
+    }
+
+
+})
+
+module.exports = router;
